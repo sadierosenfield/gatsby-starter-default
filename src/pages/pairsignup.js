de567fa2170422ccb270e51f34981fa9fbcdc162
sadierosenfield <82782722+sadierosenfield@users.noreply.github.com>
2022-02-10 10:57:46 -0800

    Update and rename page-2.js to pairsignup.js

---
+++ src/pages/pairsignup.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <div data-tf-widget="EcZio3GK" data-tf-iframe-props="title=Registration Form" data-tf-medium="snippet" style="width:100%;height:400px;"></div><script src="//embed.typeform.com/next/embed.js"></script>
+)
+
+export default SecondPage
